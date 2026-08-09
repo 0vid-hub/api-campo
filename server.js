541895462d728cbfa11ff360c3e9b1177f96616e
0vid-hub <0yd4vid@gmail.com>
2026-08-09 04:01:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,20 +14,20 @@ const BANCO_DE_CARTAS = {
   // 90-94 OVERALL
   "luka modric 94": "https://i.ibb.co/5WvWtzPq/modric94.png",
   "ronaldo 93": "https://i.ibb.co/wN7PFP96/c-ronaldo.png",
-  "mbappé 92": "https://i.ibb.co/K3sDyFd/mbappe92.png",
+  "mbappe 92": "https://i.ibb.co/K3sDyFd/mbappe92.png",
+  "pele 91": "https://i.ibb.co/HDd67r7w/pele.png",
   "courtois 90": "https://i.ibb.co/7JkNk4nW/courtois.png",
-  "pelé 91": "https://i.ibb.co/HDd67r7w/pele.png",
   "buffon 90": "https://i.ibb.co/KxxLnfWd/buffon.png",
-  "eusébio 90": "https://i.ibb.co/cSY6C7vP/eusebio.png",
+  "eusebio 90": "https://i.ibb.co/cSY6C7vP/eusebio.png",
   "lev yashin 90": "https://i.ibb.co/WWxHcr1m/yashin.png",
-  
+
   // 88-89 OVERALL
   "marcelo 89": "https://i.ibb.co/WW2Ddt99/marcelo.png",
   "pavard 89": "https://i.ibb.co/CpVYWS51/pavard.png",
   "varane 89": "https://i.ibb.co/gZxkBWMp/rapvarane.png",
   "thiago silva 89": "https://i.ibb.co/Wpv27xRF/thiagosilva.png",
   "luis figo 89": "https://i.ibb.co/Vc7HpNjF/luisfigo.png",
-  "ronaldo nazário 89": "https://i.ibb.co/gLqzcW1C/r9.png",
+  "ronaldo nazario 89": "https://i.ibb.co/gLqzcW1C/r9.png",
   "cafu 88": "https://i.ibb.co/wZhHw1Wq/cafu.png",
   "griezmann 88": "https://i.ibb.co/nsM51rXC/griesmann.png",
   "hazard 88": "https://i.ibb.co/Gf8pLcHz/hazard.png",
@@ -38,15 +38,16 @@ const BANCO_DE_CARTAS = {
   "pepe 88": "https://i.ibb.co/bMYppmWJ/pepe.png",
   "roberto carlos 88": "https://i.ibb.co/Ps3KFZ5h/robertocarlos.png",
   "zidane 88": "https://i.ibb.co/wr45wSpS/zidane.png",
-
   "lionel messi 88": "https://i.ibb.co/SD9XC7KK/messi86.png",
+
+  // 86-87 OVERALL
   "martinez 87": "https://i.ibb.co/PsTNhDxT/martinez86.png",
-  "mbappé 87": "https://i.ibb.co/wTzfxNN/mbappe86.png",
+  "mbappe 87": "https://i.ibb.co/wTzfxNN/mbappe86.png",
   "luka modric 87": "https://i.ibb.co/cXh7dVZ1/modric87.png",
   "beckenbauer 87": "https://i.ibb.co/ZzzWHWk8/beckenbauer.png",
   "george best 87": "https://i.ibb.co/Gf5nDHyc/best.png",
   "iniesta 87": "https://i.ibb.co/JFBg47m5/iniest.png",
-  "kaká 87": "https://i.ibb.co/Y7BZskxC/kaka.png",
+  "kaka 87": "https://i.ibb.co/Y7BZskxC/kaka.png",
   "paolo maldini 87": "https://i.ibb.co/GvXpVHYS/maldini.png",
   "ronaldinho 87": "https://i.ibb.co/1G1Tqxhh/ronaldinho.png",
   "xavi 87": "https://i.ibb.co/w8XTqG3/xavi.png",
@@ -54,7 +55,6 @@ const BANCO_DE_CARTAS = {
   "maradona 86": "https://i.ibb.co/YFPPdqSb/maradona.png",
   "ricardo quaresma 86": "https://i.ibb.co/k2HvS9V7/quaresma.png",
   "zlatan ibrahimovic 86": "https://i.ibb.co/Mks18Nm9/zlatan.png",
-
   "amrabat 86": "https://i.ibb.co/VP0s2cc/amrabat86.png",
   "bruno fernandes 86": "https://i.ibb.co/QjHQkGFn/brunofernandes86.png",
   "griezmann 86": "https://i.ibb.co/V0KY5RzN/griezmann86.png",
@@ -63,16 +63,16 @@ const BANCO_DE_CARTAS = {
   "pepe 86": "https://i.ibb.co/Rp9xf308/pepe86.png",
 
   // 85 OVERALL
-  "unai simón 85": "https://i.ibb.co/1tP6SR0K/unaisimon85.png",
+  "unai simon 85": "https://i.ibb.co/1tP6SR0K/unaisimon85.png",
   "rodri 85": "https://i.ibb.co/XrH16CKR/rodri85.png",
   "pedro porro 85": "https://i.ibb.co/sJCBTG0L/pedroporro85.png",
   "pedri 85": "https://i.ibb.co/Kx1WRGvw/pedri85.png",
-  "pau cubarsí 85": "https://i.ibb.co/PsrSRLgh/paucubarsi85.png",
+  "pau cubarsi 85": "https://i.ibb.co/PsrSRLgh/paucubarsi85.png",
   "pablo gavi 85": "https://i.ibb.co/4g2mTJm8/pablogavi85.png",
   "nico williams 85": "https://i.ibb.co/DgMnV7WG/nicowilliams85.png",
   "merino 85": "https://i.ibb.co/DHLYFjSH/merino85.png",
   "laporte 85": "https://i.ibb.co/NdHPPp1s/laporte85.png",
-  "joan garcía 85": "https://i.ibb.co/cq0JrjR/joangarcia85.png",
+  "joan garcia 85": "https://i.ibb.co/cq0JrjR/joangarcia85.png",
   "grimaldo 85": "https://i.ibb.co/qYP8jdbk/grimaldo85.png",
   "ferran torres 85": "https://i.ibb.co/tpY91PsB/ferrantorres85.png",
   "david raya 85": "https://i.ibb.co/nqb1MhDL/davidraya85.png",
@@ -86,13 +86,13 @@ const BANCO_DE_CARTAS = {
   "bellingham 84": "https://i.ibb.co/f31P2Vq/bellingham.png",
   "haaland 84": "https://i.ibb.co/yBS6Z4s0/haaland84.png",
   "harry kane 84": "https://i.ibb.co/mF5fFLHR/harrykane.png",
-  "mbappé 84": "https://i.ibb.co/m5PjypFT/mbappe84.png",
+  "mbappe 84": "https://i.ibb.co/m5PjypFT/mbappe84.png",
   "neymar jr 84": "https://i.ibb.co/ZpdBqzxF/neymar84.png",
   "nuno mendes 84": "https://i.ibb.co/67yHcGp3/nunomendes84.png",
   "vozinha 84": "https://i.ibb.co/PZyC4rDs/vozinha84.png",
 
   // 80 - 83 OVERALL
-  "vinicius júnior 83": "https://i.ibb.co/KMnsD2j/vini83.png",
+  "vinicius junior 83": "https://i.ibb.co/KMnsD2j/vini83.png",
   "luka modric 83": "https://i.ibb.co/zWpt7p4w/modric83.png",
   "michael olise 83": "https://i.ibb.co/9HVsPRfg/olise.png",
   "ronaldo 83": "https://i.ibb.co/B2vyBJj1/ronaldo83.png",
@@ -100,19 +100,19 @@ const BANCO_DE_CARTAS = {
   "diogo costa 83": "https://i.ibb.co/gLkfnyvc/diogocosta83.png",
   "khvicha kvaratskhelia 82": "https://i.ibb.co/1GqXhm5N/kvara82.png",
   "vitinha 82": "https://i.ibb.co/Kj7B9f57/vitinha82.png",
-  "joão neves 81": "https://i.ibb.co/mCvgB2hj/joaoneves81.png",
-  "rafael leão 81": "https://i.ibb.co/CKjMSjtJ/rafaleao81.png",
+  "joao neves 81": "https://i.ibb.co/mCvgB2hj/joaoneves81.png",
+  "rafael leao 81": "https://i.ibb.co/CKjMSjtJ/rafaleao81.png",
   "bruno fernandes 80": "https://i.ibb.co/HpBJgxrb/brunofernandes80.png",
   "ruben dias 80": "https://i.ibb.co/SLNg0bV/rubendias80.png",
-  "gonçalo ramos 80": "https://i.ibb.co/sJXr8Yd1/gon-aloramos80.png",
+  "goncalo ramos 80": "https://i.ibb.co/sJXr8Yd1/gon-aloramos80.png",
   "neymar jr 80": "https://i.ibb.co/WNX88Xj1/neymar80.png",
 
   // 75 - 79 OVERALL
   "rodri 79": "https://i.ibb.co/NntbtcYh/rodri79r.png",
-  "vinicius júnior 78": "https://i.ibb.co/k6r70yqc/vini78r.png",
+  "vinicius junior 78": "https://i.ibb.co/k6r70yqc/vini78r.png",
   "kevin de bruyne 78": "https://i.ibb.co/PsFCVnHg/DEBRUYNE78r.png",
   "lamine yamal 78": "https://i.ibb.co/rRR5jFSz/yamal78r.png",
-  "mbappé 78": "https://i.ibb.co/gMy5ZqkZ/mbappe78r.png",
+  "mbappe 78": "https://i.ibb.co/gMy5ZqkZ/mbappe78r.png",
   "lionel messi 77": "https://i.ibb.co/RTTnXHHB/messi77r.png",
   "harry kane 77": "https://i.ibb.co/7tbgyhq5/kane77r.png",
   "bellingham 76": "https://i.ibb.co/21kPP49k/bellingham76r.png",
@@ -127,23 +127,23 @@ const BANCO_DE_CARTAS = {
   // 70 - 74 OVERALL
   "cucurella 74": "https://i.ibb.co/M52XXn3H/cucurella74.png",
   "endrick 74": "https://i.ibb.co/jvzs7j4q/endrick74.png",
-  "estevão 74": "https://i.ibb.co/LdyFYYnK/estevao74.png",
+  "estevao 74": "https://i.ibb.co/LdyFYYnK/estevao74.png",
   "gyokeres 74": "https://i.ibb.co/TM2f0Mgg/gyokeres74.png",
-  "joão cancelo 74": "https://i.ibb.co/KcvmXmKR/joaocancelo75.png",
+  "joao cancelo 74": "https://i.ibb.co/KcvmXmKR/joaocancelo75.png",
   "vitor roque 74": "https://i.ibb.co/yMPG18g/vitorroque74.png",
   "wendell 74": "https://i.ibb.co/fV2QtJSy/wendell74.png",
   "evanilson 74": "https://i.ibb.co/nN2cMbWF/evanilson74.png",
   "hwand din beom 74": "https://i.ibb.co/k2yKSdX0/hwandinbeom74.png",
-  "kaio césar 74": "https://i.ibb.co/Szqt9VW/kaioc-sar74.png",
-  "orkun kokçu 74": "https://i.ibb.co/k2CB7Jyc/kok-u74.png",
-  "richard ríos 74": "https://i.ibb.co/Dfm9pqf1/richardrios74.png",
+  "kaio cesar 74": "https://i.ibb.co/Szqt9VW/kaioc-sar74.png",
+  "orkun kokcu 74": "https://i.ibb.co/k2CB7Jyc/kok-u74.png",
+  "richard rios 74": "https://i.ibb.co/Dfm9pqf1/richardrios74.png",
   "virgil van dijk 74": "https://i.ibb.co/B5TJ9ZP7/vandik74.png",
   "igor thiago 73": "https://i.ibb.co/qY9k3vDK/igorthiago73.png",
   "weston mckennie 73": "https://i.ibb.co/8DvLvMMZ/westonmckennie73.png",
   "alejandro garnacho 73": "https://i.ibb.co/ksYjsC4f/garnacho73.png",
   "facundo torres 73": "https://i.ibb.co/rf2LCz8t/facundotorres73.png",
   "tiago santos 72": "https://i.ibb.co/dJ3j3ThW/tiago-santos72.png",
-  "gonçalo borges 71": "https://i.ibb.co/gbgtnk79/gon-aloborges71.png",
+  "goncalo borges 71": "https://i.ibb.co/gbgtnk79/gon-aloborges71.png",
   "pavlidis 71": "https://i.ibb.co/nNwYZXhW/pavlidis71.png",
   "samuel portugal 70": "https://i.ibb.co/0y7cHN8d/samuelportugal74.png",
   "trubin 70": "https://i.ibb.co/q3nBxygp/trubin70.png",
@@ -154,37 +154,49 @@ const BANCO_DE_CARTAS = {
   // 65 - 69 OVERALL
   "carlinhos 69": "https://i.ibb.co/6cXqTrRP/carlinhos69r.png",
   "francisco moura 69": "https://i.ibb.co/1YtgK2QV/franciscomoura69r.png",
-  "gonçalo sá 69": "https://i.ibb.co/2Yd1XYnz/gon-alosa69r.png",
-  "joão ferreira 69": "https://i.ibb.co/WW78PcRq/joaoferreira69r.png",
+  "goncalo sa 69": "https://i.ibb.co/2Yd1XYnz/gon-alosa69r.png",
+  "joao ferreira 69": "https://i.ibb.co/WW78PcRq/joaoferreira69r.png",
   "marcos leonardo 69": "https://i.ibb.co/sJWTH9s1/marcosleonardo69r.png",
   "matheusinho 69": "https://i.ibb.co/yBF1cnvD/matheusinho69r.png",
   "nathan silva 69": "https://i.ibb.co/s95s7B2z/nathansilva69r.png",
   "paulo bernardo 69": "https://i.ibb.co/mF0tHxDv/paulobernardo69r.png",
   "rodrigo riquelme 69": "https://i.ibb.co/LX1Y5XSt/rodrigoriqualme69r.png",
-  "tomás ribeiro 69": "https://i.ibb.co/Zpyg3Bnb/tomasribeiro68r.png",
+  "tomas ribeiro 69": "https://i.ibb.co/Zpyg3Bnb/tomasribeiro68r.png",
   "tiquinho soares 69": "https://i.ibb.co/bRF6S1Jq/tiquinhosoares69r.png",
-  "josé sá 69": "https://i.ibb.co/JWB6tzmd/josesa69r.png",
-  "gonçalo tabuaço 68": "https://i.ibb.co/390G8ZGT/gon-alotuabua-o68r.png",
-  "henrique araújo 68": "https://i.ibb.co/fYQNHLkw/henriquearaujo69r.png",
+  "jose sa 69": "https://i.ibb.co/JWB6tzmd/josesa69r.png",
+  "goncalo tabuaco 68": "https://i.ibb.co/390G8ZGT/gon-alotuabua-o68r.png",
+  "henrique araujo 68": "https://i.ibb.co/fYQNHLkw/henriquearaujo69r.png",
   "rodrigo pinho 67": "https://i.ibb.co/hR1Cvs0f/rodrigopinho67r.png",
-  "tomas araújo 67": "https://i.ibb.co/LXTybjKP/tomasaraujo67r.png",
-  "fábio vieira 66": "https://i.ibb.co/JFWGB85y/fabiovieira66r.png",
+  "tomas araujo 67": "https://i.ibb.co/LXTybjKP/tomasaraujo67r.png",
+  "fabio vieira 66": "https://i.ibb.co/JFWGB85y/fabiovieira66r.png",
   "nuno tavares 65": "https://i.ibb.co/84Lqjh54/nunotacares65r.png",
-  "joão mário 65": "https://i.ibb.co/6jwS6H8/joaomario65r.png",
+  "joao mario 65": "https://i.ibb.co/6jwS6H8/joaomario65r.png",
   "toti gomes 65": "https://i.ibb.co/WWZ1hRSR/totigomes65r.png",
 
   // 60 - 64 OVERALL
   "marcano 64": "https://i.ibb.co/ymTyPkY1/marcano64.png",
   "lukas ullrich 64": "https://i.ibb.co/Kj7B9f57/lukasullrish64.png",
-  "andré almeida 63": "https://i.ibb.co/Y7PGCHR3/andrealmeida63.png",
+  "andre almeida 63": "https://i.ibb.co/Y7PGCHR3/andrealmeida63.png",
   "nico schlotterbeck 63": "https://i.ibb.co/MkSDdfLz/nico63.png",
   "de la cruz 62": "https://i.ibb.co/SwjyzpmJ/delacruz62.png",
   "alexander bah 62": "https://i.ibb.co/fVB5JCPR/bah62.png",
   "charles 60": "https://i.ibb.co/gMTdxy9D/charles60.png",
   "joaquin lavega 60": "https://i.ibb.co/wZHHhNkR/joaquinlavega60.png",
-  "caio césar 60": "https://i.ibb.co/67n1Lhkb/caiocesar60.png",
-  "caça rato 60": "https://i.ibb.co/hRNn6wPv/ca-a-rato60.png",
-  "zé ricardo 60": "https://i.ibb.co/G3C6JhmD/zericardo60.png"
+  "caio cesar 60": "https://i.ibb.co/67n1Lhkb/caiocesar60.png",
+  "caca rato 60": "https://i.ibb.co/hRNn6wPv/ca-a-rato60.png",
+  "ze ricardo 60": "https://i.ibb.co/G3C6JhmD/zericardo60.png"
+};
+
+// Tabela Hash O(1) de Preços por OVERALL
+const TABELA_PRECOS = {
+  99: 100000, 98: 80000, 97: 65000, 96: 52000, 95: 42000,
+  94: 34000,  93: 28000, 92: 23000, 91: 19000, 90: 16000,
+  89: 13500,  88: 11500, 87: 9800,  86: 8300,  85: 7000,
+  84: 5800,   83: 4800,  82: 3900,  81: 3100,  80: 2500,
+  79: 2100,   78: 1800,  77: 1550,  76: 1350,  75: 1200,
+  74: 1050,   73: 920,   72: 810,   71: 710,   70: 620,
+  69: 540,    68: 470,   67: 410,   66: 360,   65: 310,
+  64: 270,    63: 240,   62: 210,   61: 180
 };
 
 function removerAcentos(texto) {
@@ -198,6 +210,26 @@ function removerAcentos(texto) {
     .replace(/[\u0300-\u036f]/g, "")
     .toLowerCase()
     .trim();
+}
+
+function obterPrecoPorOverall(ovr) {
+  if (ovr <= 60) return 150;
+  return TABELA_PRECOS[ovr] || 1000;
+}
+
+// Algoritmo de busca por nome (Exata -> Contém)
+function buscarChaveJogador(busca) {
+  const buscaLimpa = removerAcentos(busca);
+  if (!buscaLimpa) return null;
+
+  const chaves = Object.keys(BANCO_DE_CARTAS);
+
+  // 1. Tenta correspondência exata primeiro
+  const exata = chaves.find(nome => removerAcentos(nome) === buscaLimpa);
+  if (exata) return exata;
+
+  // 2. Se não achar exata, busca se o nome contém o termo buscado
+  return chaves.find(nome => removerAcentos(nome).includes(buscaLimpa)) || null;
 }
 
 // ROTA 1: GERAR IMAGEM DO CAMPO
@@ -235,24 +267,23 @@ app.get('/gerar-campo', async (req, res) => {
     };
 
     for (const [pos, coord] of Object.entries(POSICOES)) {
-      const busca = removerAcentos(req.query[pos]);
+      const valorQuery = req.query[pos];
+      if (!valorQuery || valorQuery === 'vazio') continue;
 
-      if (busca && busca !== 'vazio') {
-        const chaveEncontrada = Object.keys(BANCO_DE_CARTAS).find(nome => removerAcentos(nome).includes(busca));
+      const chaveEncontrada = buscarChaveJogador(valorQuery);
 
-        if (chaveEncontrada && BANCO_DE_CARTAS[chaveEncontrada]) {
-          try {
-            const cardImg = await loadImage(BANCO_DE_CARTAS[chaveEncontrada]);
-            ctx.drawImage(
-              cardImg, 
-              coord.x - cardWidth / 2, 
-              coord.y - cardHeight / 2, 
-              cardWidth, 
-              cardHeight
-            );
-          } catch (err) {
-            console.error(`Erro ao carregar imagem para ${busca}:`, err.message);
-          }
+      if (chaveEncontrada && BANCO_DE_CARTAS[chaveEncontrada]) {
+        try {
+          const cardImg = await loadImage(BANCO_DE_CARTAS[chaveEncontrada]);
+          ctx.drawImage(
+            cardImg, 
+            coord.x - cardWidth / 2, 
+            coord.y - cardHeight / 2, 
+            cardWidth, 
+            cardHeight
+          );
+        } catch (err) {
+          console.error(`Erro ao carregar imagem para ${valorQuery}:`, err.message);
         }
       }
     }
@@ -272,21 +303,7 @@ app.get('/buscar-jogador', (req, res) => {
 
   try {
     const queryBruta = req.query.q || "";
-    const buscaLimpa = removerAcentos(queryBruta);
-
-    if (!buscaLimpa) {
-      return res.status(200).json({ 
-        sucesso: false, 
-        erro: "busca_vazia",
-        imagem: "https://i.ibb.co/sd3x55sR/desconhecido.png",
-        overall: 60 
-      });
-    }
-
-    const chaveEncontrada = Object.keys(BANCO_DE_CARTAS).find(nomeNoBanco => {
-      const nomeLimpo = removerAcentos(nomeNoBanco);
-      return nomeLimpo.includes(buscaLimpa);
-    });
+    const chaveEncontrada = buscarChaveJogador(queryBruta);
 
     if (!chaveEncontrada) {
       return res.status(200).json({ 
@@ -300,48 +317,7 @@ app.get('/buscar-jogador', (req, res) => {
     const partes = chaveEncontrada.split(' ');
     const overall = parseInt(partes[partes.length - 1]) || 60;
     const imagem = BANCO_DE_CARTAS[chaveEncontrada];
-
-    let preco = 1000;
-    if (overall === 99) preco = 100000;
-    else if (overall === 98) preco = 80000;
-    else if (overall === 97) preco = 65000;
-    else if (overall === 96) preco = 52000;
-    else if (overall === 95) preco = 42000;
-    else if (overall === 94) preco = 34000;
-    else if (overall === 93) preco = 28000;
-    else if (overall === 92) preco = 23000;
-    else if (overall === 91) preco = 19000;
-    else if (overall === 90) preco = 16000;
-    else if (overall === 89) preco = 13500;
-    else if (overall === 88) preco = 11500;
-    else if (overall === 87) preco = 9800;
-    else if (overall === 86) preco = 8300;
-    else if (overall === 85) preco = 7000;
-    else if (overall === 84) preco = 5800;
-    else if (overall === 83) preco = 4800;
-    else if (overall === 82) preco = 3900;
-    else if (overall === 81) preco = 3100;
-    else if (overall === 80) preco = 2500;
-    else if (overall === 79) preco = 2100;
-    else if (overall === 78) preco = 1800;
-    else if (overall === 77) preco = 1550;
-    else if (overall === 76) preco = 1350;
-    else if (overall === 75) preco = 1200;
-    else if (overall === 74) preco = 1050;
-    else if (overall === 73) preco = 920;
-    else if (overall === 72) preco = 810;
-    else if (overall === 71) preco = 710;
-    else if (overall === 70) preco = 620;
-    else if (overall === 69) preco = 540;
-    else if (overall === 68) preco = 470;
-    else if (overall === 67) preco = 410;
-    else if (overall === 66) preco = 360;
-    else if (overall === 65) preco = 310;
-    else if (overall === 64) preco = 270;
-    else if (overall === 63) preco = 240;
-    else if (overall === 62) preco = 210;
-    else if (overall === 61) preco = 180;
-    else if (overall <= 60) preco = 150;
+    const preco = obterPrecoPorOverall(overall);
 
     return res.status(200).json({
       sucesso: true,
@@ -425,6 +401,21 @@ app.get('/simular-partida', async (req, res) => {
   const webhookUrl = `https://discord.com/api/webhooks/${webhookIdToken}`;
   const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+  // Função auxiliar para requisições seguras ao Discord com tratamento de Rate-Limit (HTTP 429)
+  const patchEmbedDiscord = async (msgId, dataEmbed) => {
+    try {
+      await axios.patch(`${webhookUrl}/messages/${msgId}`, dataEmbed);
+    } catch (err) {
+      if (err.response && err.response.status === 429) {
+        const retryAfter = (err.response.data.retry_after || 1) * 1000;
+        await sleep(retryAfter);
+        await axios.patch(`${webhookUrl}/messages/${msgId}`, dataEmbed);
+      } else {
+        console.error("Erro ao atualizar webhook do Discord:", err.message);
+      }
+    }
+  };
+
   try {
     const gC = Number(gerCasa) || 60;
     const gF = Number(gerFora) || 60;
@@ -437,7 +428,14 @@ app.get('/simular-partida', async (req, res) => {
       fields: [{ name: "Placar", value: `**${timeCasa} 0 x 0 ${timeFora}**` }]
     };
 
-    const resWebhook = await axios.post(`${webhookUrl}?wait=true`, { embeds: [embedInicial] });
+    let resWebhook;
+    try {
+      resWebhook = await axios.post(`${webhookUrl}?wait=true`, { embeds: [embedInicial] });
+    } catch (whErr) {
+      console.error("Falha ao criar mensagem inicial no Webhook:", whErr.message);
+      return res.status(500).json({ sucesso: false, erro: "falha_webhook_inicial" });
+    }
+
     const messageId = resWebhook.data.id;
 
     let golsCasa = 0;
@@ -471,7 +469,7 @@ app.get('/simular-partida', async (req, res) => {
           ]
         };
 
-        await axios.patch(`${webhookUrl}/messages/${messageId}`, { embeds: [embedProgresso] });
+        await patchEmbedDiscord(messageId, { embeds: [embedProgresso] });
       } else {
         // Fim de jogo
         let resultadoTexto = "🤝 **Empate!**";
@@ -488,7 +486,7 @@ app.get('/simular-partida', async (req, res) => {
           footer: { text: "ES League — Partida Finalizada" }
         };
 
-        await axios.patch(`${webhookUrl}/messages/${messageId}`, { embeds: [embedFinal] });
+        await patchEmbedDiscord(messageId, { embeds: [embedFinal] });
       }
     }
 
@@ -496,7 +494,7 @@ app.get('/simular-partida', async (req, res) => {
     return res.status(200).json({ sucesso: true, mensagem: "Partida finalizada com sucesso!" });
 
   } catch (err) {
-    console.error("Erro na simulação:", err.response ? err.response.data : err.message);
+    console.error("Erro na simulação:", err.message);
     if (!res.headersSent) {
       return res.status(500).json({ sucesso: false, erro: "erro_simulacao" });
     }
